53047c478576e22f22ffbc3b1e7d46536c0066a2
MohamedHassan30 <mohamedanour303@gmail.com>
2022-12-24 01:44:49 +0200

    we made a widget Called SingleChildScrollView above column

--- lib/login,dart/login_screen.dart
+++ lib/login,dart/login_screen.dart
@@ -18,36 +18,38 @@ class _LoginScreenState extends State<LoginScreen> {
     return Scaffold(
       body: Padding(
         padding: EdgeInsets.all(10),
-        child :Column(children: [
-        SizedBox(height: 200,),
-        MyTextField(hintText: "Enter Your Email", textInputType: TextInputType.emailAddress, obscureText: false),
-       SizedBox(height: 20,),
-       MyTextField(hintText: "Enter Your Password", textInputType: TextInputType.visiblePassword, obscureText: true),
-       SizedBox(height: 50,),
-       ElevatedButton(onPressed: (){
-        Navigator.push(context, MaterialPageRoute(builder: (context)=> HomeScreen()));
-       }, child: Text("Sign in",
-       style: TextStyle(fontSize: 18),
-       
-       ),
-       style: ButtonStyle(
-        backgroundColor: MaterialStateProperty.all(Colors.green,),
-        shape: MaterialStateProperty.all(RoundedRectangleBorder(
-          borderRadius: BorderRadius.circular(10),
-        ),),
-        padding: MaterialStateProperty.all(EdgeInsets.all(8),),
-       ),),
-       SizedBox(height: 30,),
-       Row(
-        children: [
-          Text("Do not have any account ?",style: TextStyle(fontSize: 18),),
-          TextButton(onPressed: (){
-            Navigator.push(context, MaterialPageRoute(builder: (context)=> RegisterScreen()));
-          }, child: Text("Sign Up",style: TextStyle(fontSize: 18,fontWeight: FontWeight.bold, color: Colors.green),)),
-        ],
-       ),
-
-      ]),
+        child :SingleChildScrollView(
+          child: Column(children: [
+          SizedBox(height: 200,),
+          MyTextField(hintText: "Enter Your Email", textInputType: TextInputType.emailAddress, obscureText: false),
+               SizedBox(height: 20,),
+               MyTextField(hintText: "Enter Your Password", textInputType: TextInputType.visiblePassword, obscureText: true),
+               SizedBox(height: 50,),
+               ElevatedButton(onPressed: (){
+          Navigator.push(context, MaterialPageRoute(builder: (context)=> HomeScreen()));
+               }, child: Text("Sign in",
+               style: TextStyle(fontSize: 18),
+               
+               ),
+               style: ButtonStyle(
+          backgroundColor: MaterialStateProperty.all(Colors.green,),
+          shape: MaterialStateProperty.all(RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(10),
+          ),),
+          padding: MaterialStateProperty.all(EdgeInsets.all(8),),
+               ),),
+               SizedBox(height: 30,),
+               Row(
+          children: [
+            Text("Do not have any account ?",style: TextStyle(fontSize: 18),),
+            TextButton(onPressed: (){
+              Navigator.push(context, MaterialPageRoute(builder: (context)=> RegisterScreen()));
+            }, child: Text("Sign Up",style: TextStyle(fontSize: 18,fontWeight: FontWeight.bold, color: Colors.green),)),
+          ],
+               ),
+        
+              ]),
+        ),
       ),
       
     );
