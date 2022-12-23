82d1736a2ccfd60e084cf8df5752ddc38e7efc3f
MohamedHassan30 <mohamedanour303@gmail.com>
2022-12-24 01:23:21 +0200

    checkout_screen

--- lib/component.dart/main,dart/checkout_screen.dart
+++ lib/component.dart/main,dart/checkout_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/container.dart';
 import 'package:flutter/src/widgets/framework.dart';
+import 'package:flutter_application_1/component.dart/main,dart/home_screen.dart';
 import 'package:flutter_application_1/provider/cart.dart';
 import 'package:provider/provider.dart';
 
@@ -12,7 +13,7 @@ class CheckoutScreen extends StatefulWidget {
 }
 
 class _CheckoutScreenState extends State<CheckoutScreen> {
-  
+ 
   @override
   Widget build(BuildContext context) {  
     return Scaffold(
@@ -36,7 +37,9 @@ class _CheckoutScreenState extends State<CheckoutScreen> {
                   decoration: BoxDecoration(
                   
                     color: Color.fromARGB(211, 164, 255, 193),
-                   shape:BoxShape.circle ),
+                  shape:BoxShape.circle
+                  )
+                
                 ),
               IconButton(onPressed: (){
                                 Navigator.push(context, MaterialPageRoute(builder: ((context) => CheckoutScreen())));
@@ -57,41 +60,52 @@ class _CheckoutScreenState extends State<CheckoutScreen> {
     ],),
     
   
-   body: Consumer<Cart>(
-    builder: ((context, value, child) {
-      return  Column(
-       children: [
-        ListView.separated(
-          itemBuilder: ((context, index) {
-          return ListTile(
-          subtitle: Text("Test"),
-          title: Text("Test"),
-          leading: CircleAvatar(
-            backgroundImage: AssetImage('${value.SelectedProducts[index].imagePath}')
-           
-            
-          ),
-          trailing:IconButton(onPressed: () {
+   body: Container(
+        height: 700,
+        child: Column(
+         children: [
+          Consumer<Cart>(
+            builder: (context, value, child) {
+             return ListView.separated(
+              physics: BouncingScrollPhysics(),
+              scrollDirection: Axis.vertical,
+              shrinkWrap: true,
+              itemBuilder: ((context, index) {
               
+                 return Container(
+                  height: 50,
+                  width: double.infinity,
+                  child: ListTile(
+                  subtitle: Text("${value.SelectedProducts[index].Price}"),
+                  title: Text("${value.SelectedProducts[index].Price}"),
+                  leading: CircleAvatar(
+                    backgroundImage: AssetImage('${value.SelectedProducts[index].imagePath}')
+                   
+                    
+                  ),
+                  trailing:IconButton(onPressed: () {
+                      
+                    },
+                    icon: Icon(Icons.remove), 
+                    ),
+                         ),
+                );
+                
+            }), separatorBuilder: ((context, index) {
+              return SizedBox(
+                height: 20,
+              );
+            }), itemCount:value.SelectedProducts.length ,
+            );
             },
-            icon: Icon(Icons.remove), 
-            ),
-         );
-        }), separatorBuilder: ((context, index) {
-          return SizedBox(
-            height: 5,
-          );
-        }), itemCount: value.SelectedProducts.length,
-        )
-        
+            
+          )
+          
    
-       ],
-     );
-    }),
-     
-
-
-   ), 
+         ],
+     ),
+      ),
+    
 
 
     );
